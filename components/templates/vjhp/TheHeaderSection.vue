88ba128c2df6cfa03e2565643c3a29fe994dd856
Dan Weather <spamdrap@gmail.com>
2020-08-26 22:58:38 +0800

    Add fields

--- components/templates/vjhp/TheHeaderSection.vue
+++ components/templates/vjhp/TheHeaderSection.vue
@@ -2,7 +2,7 @@
   <div id="wrapper">
     <header id="header">
       <span class="logo">
-        <img src="/marketing/img/vjhp/logo.svg" alt />
+        <img src="~assets/images/vjhp/logo.svg?data" alt="Vera&John" />
       </span>
       <h2>ベラジョン・オンラインカジノへようこそ!</h2>
       <h1>日本NO.1カジノ!</h1>
@@ -12,47 +12,46 @@
       </h3>
       <ul class="items-list gameslider">
         <li>
-          <a href="#">
-            <img src="/marketing/img/vjhp/game1.svg" />
+          <a href="https://www.verajohn.com/home">
+            <img src="~assets/images/vjhp/game1.svg?data" alt="game1"/>
           </a>
         </li>
         <li>
-          <a href="#">
-            <img src="/marketing/img/vjhp/game2.svg" />
+          <a href="https://www.verajohn.com/home">
+            <img src="~assets/images/vjhp/game2.svg?data" alt="game2"/>
           </a>
         </li>
         <li>
-          <a href="#">
-            <img src="/marketing/img/vjhp/game3.svg" />
+          <a href="https://www.verajohn.com/home">
+            <img src="~assets/images/vjhp/game3.svg?data" alt="game2"/>
           </a>
         </li>
         <li>
-          <a href="#">
-            <img src="/marketing/img/vjhp/game4.svg" />
+          <a href="https://www.verajohn.com/home">
+            <img src="~assets/images/vjhp/game4.svg?data" alt="game4"/>
           </a>
         </li>
         <li>
-          <a href="#">
-            <img src="/marketing/img/vjhp/game5.svg" />
+          <a href="https://www.verajohn.com/home">
+            <img src="~assets/images/vjhp/game5.svg?data" alt="game5"/>
           </a>
         </li>
         <li>
-          <a href="#">
-            <img src="/marketing/img/vjhp/game6.svg" />
+          <a href="https://www.verajohn.com/home">
+            <img src="~assets/images/vjhp/game6.svg?data" alt="game6"/>
           </a>
         </li>
       </ul>
       <h4>登録してからすぐ使える! 業界一お得なビギナーズボーナスのおまけつき!</h4>
       <div class="chest">
-        <a href="#">
-          <img src="/marketing/img/vjhp/chest.svg" />
+        <a href="https://www.verajohn.com/home">
+          <img src="~assets/images/vjhp/chest.svg?data" alt="chest"/>
         </a>
       </div>
       <p>お試し登録ボーナス &amp; 最初3回のご入金に合計最大$1000ボーナスをプレゼント!</p>
       <a class="button primary" v-bind:href="header_button_redirect_url">{{ header_text }}</a>
       <p>
-        アカウントをお持ちの方はコチラから
-        <a v-bind:href="header_login_account_text">{{ header_login_account_text_redirect_url }}</a>
+        アカウントをお持ちの方はコチラから<a v-bind:href="header_login_account_text_redirect_url">{{ header_login_account_text }}</a>
       </p>
     </header>
   </div>
@@ -62,29 +61,100 @@
 export default {
   name: 'TheHeaderSection',
   props: {
-    header_text: {
+    // header_text: {
+    //   type: String,
+    //   required: true,
+    // },
+    // header_button_redirect_url: {
+    //   type: String,
+    //   required: true,
+    // },
+    // header_login_account_text: {
+    //   type: String,
+    //   required: true,
+    // },
+    // header_login_account_text_redirect_url: {
+    //   type: String,
+    //   required: true,
+    // },
+    // homepage_title: {
+    //   type: String,
+    //   required: true,
+    // },
+    login_header_text: {
       type: String,
       required: true,
     },
-    header_button_redirect_url: {
+login_header_redirect_url: {
       type: String,
       required: true,
     },
-    header_login_account_text: {
+header_section_welcome_message: {
       type: String,
       required: true,
     },
-    header_login_account_text_redirect_url: {
+header_section_main_heading: {
       type: String,
       required: true,
     },
-    homepage_title: {
+header_section_heading_subtitle: {
       type: String,
       required: true,
     },
-  },
+header_section_game_slider_url_1: {
+      type: String,
+      required: true,
+    },
+header_section_game_slider_url_2: {
+      type: String,
+      required: true,
+    },
+header_section_game_slider_url_3: {
+      type: String,
+      required: true,
+    },
+header_section_game_slider_url_4: {
+      type: String,
+      required: true,
+    },
+header_section_game_slider_url_5: {
+      type: String,
+      required: true,
+    },
+header_section_game_slider_url_6: {
+      type: String,
+      required: true,
+    },
+header_section_best_deal_caption: {
+      type: String,
+      required: true,
+    },
+header_section_best_deal_small_caption: {
+      type: String,
+      required: true,
+    },
+header_section_register_button: {
+      type: String,
+      required: true,
+    },
+header_section_register_button_url: {
+      type: String,
+      required: true,
+    },
+header_section_account_login: {
+      type: String,
+      required: true,
+    },
+header_section_account_login_url: {
+      type: String,
+      required: true,
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
+#wrapper {
+  background-image: url("~assets/images/vjhp/bg.svg?data");
+}
 </style>
