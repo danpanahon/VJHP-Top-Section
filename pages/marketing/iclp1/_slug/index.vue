74ae69b8426e1e8c84b1cd53c9918fe24b183fbb
Dan Weather <spamdrap@gmail.com>
2020-08-26 16:20:36 +0800

    Initial files from DS gitlab

---
+++ pages/marketing/iclp1/_slug/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <TheHeader 
+      :title = "post.attributes.promo_banner.promo_title"
+      :title_display = "post.attributes.promo_banner.promo_title_display"
+      :subtitle1 = "post.attributes.promo_banner.promo_subtitle_1"
+      :main_heading = "post.attributes.promo_banner.promo_main_heading"
+      :title_animate = "post.attributes.promo_banner.promo_title_animate"
+      :subtitle2 = "post.attributes.promo_banner.promo_subtitle_2"
+      :login_button = "post.attributes.promo_banner.promo_login_button_text"
+      :login_button_redirect_url = "post.attributes.promo_banner.promo_login_button_redirect_url"
+      :signup_button = "post.attributes.promo_banner.promo_signup_button"
+      :signup_button_redirect_url = "post.attributes.promo_banner.promo_signup_button_redirect_url"
+      :small_term_text = "post.attributes.promo_banner.promo_smallterm_text"
+      :small_term_text_link = "post.attributes.promo_banner.promo_smallterm_text_link"
+      :small_term_redirect_url = "post.attributes.promo_banner.promo_smallterm_redirect_url" 
+      :images = "post.attributes.promo_banner.promo_images"
+    />
+    <TheSteps 
+      :title1 = "post.attributes.promo_steps.promo_step_1_title"
+      :description1 = "post.attributes.promo_steps.promo_step_1_desc"
+      :title2 = "post.attributes.promo_steps.promo_step_2_title"
+      :description2 = "post.attributes.promo_steps.promo_step_2_desc"
+      :title3 = "post.attributes.promo_steps.promo_step_3_title"
+      :description3 = "post.attributes.promo_steps.promo_step_3_desc"
+    />
+    <ThePayment
+      :promo_language_code = "post.attributes.promo_locale.promo_language_code"
+      :promo_country_code = "post.attributes.promo_locale.promo_country_code" />
+    <TheBody :promo_content = "post.html"/>
+    <TheFooter 
+      :promo_language_code = "post.attributes.promo_locale.promo_language_code"
+      :promo_country_code = "post.attributes.promo_locale.promo_country_code" />
+  </div>
+</template>
+
+<script>
+import TheHeader from '~/components/templates/iclp1/TheHeader.vue'
+import TheSteps from '~/components/templates/iclp1/TheSteps.vue'
+import TheBody from '~/components/templates/iclp1/TheBody.vue'
+import ThePayment from '~/components/templates/iclp1/ThePayment.vue'
+import TheFooter from '~/components/templates/TheFooter.vue'
+
+export default {
+  layout: 'iclp1',
+  head() {
+    return { 
+      title: 'InterCasino',
+      htmlAttrs: {
+        lang: this.post.attributes.promo_locale.promo_language_code
+      },
+      bodyAttrs: {
+        id: this.post.attributes.promo_locale.promo_language_code+
+        '-'+this.post.attributes.promo_locale.promo_country_code
+      }
+    } 
+  },
+  components: {
+    TheHeader,
+    TheSteps,
+    TheBody,
+    ThePayment,
+    TheFooter
+  },
+  async asyncData ({ params }) {
+    try {
+      const post = await import('~/assets/content/landing-page/marketing/iclp1/'+ params.slug +'.md')
+      return {
+        post
+      }
+    } catch (error) {
+      return false
+    }
+  }
+}
+</script>
---
+++ pages/marketing/vjhp/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <TheLoginHeaderSection
+      :title="post.attributes.promo_banner.promo_title"
+      :login_header_text="post.attributes.login_header_section.login_header_text"
+      :login_header_redirect_url="post.attributes.login_header_section.login_header_redirect_url"
+    />
+    <TheHeaderSection
+      :homepage_title="post.attributes.header_section.homepage_title || '' "
+      :header_text="post.attributes.header_section.header_text"
+      :header_button_redirect_url="post.attributes.header_section.header_button_redirect_url"
+      :header_login_account_text="post.attributes.header_section.header_login_account_text"
+      :header_login_account_text_redirect_url="post.attributes.header_section.header_login_account_text_redirect_url"
+    />
+    <TheSection2 />
+    <TheGamesBrickSection
+      :games_brick_column_1_button="post.attributes.games_brick_section.games_brick_column_1.games_brick_column_1_button"
+      :games_brick_column_2_button="post.attributes.games_brick_section.games_brick_column_2.games_brick_column_2_button"
+      :games_brick_column_1_button_redirect_url="post.attributes.games_brick_section.games_brick_column_1.games_brick_column_1_button_redirect_url"
+      :games_brick_column_2_button_redirect_url="post.attributes.games_brick_section.games_brick_column_2.games_brick_column_2_button_redirect_url"
+    />
+    <TheSection4 />
+    <TheSection5 />
+    <TheTestimonialSection
+      :testimonials_button_text="post.attributes.testimonials_section.testimonials_button_text"
+      :testimonials_button_redirect_url="post.attributes.testimonials_section.testimonials_button_redirect_url"
+    />
+    <TheSection7 />
+    <TheFooterSection
+      :footer_menu_1_text="post.attributes.footer_section.footer_menu_1_text"
+      :footer_menu_2_text="post.attributes.footer_section.footer_menu_2_text"
+      :footer_menu_3_text="post.attributes.footer_section.footer_menu_3_text"
+      :footer_menu_4_text="post.attributes.footer_section.footer_menu_4_text"
+      :footer_menu_1_redirect_url="post.attributes.footer_section.footer_menu_1_redirect_url"
+      :footer_menu_2_redirect_url="post.attributes.footer_section.footer_menu_2_redirect_url"
+      :footer_menu_3_redirect_url="post.attributes.footer_section.footer_menu_3_redirect_url"
+      :footer_menu_4_redirect_url="post.attributes.footer_section.footer_menu_4_redirect_url"
+    />
+  </div>
+</template>
+
+<script>
+import TheLoginHeaderSection from '~/components/templates/vjhp/TheLoginHeaderSection.vue';
+import TheHeaderSection from '~/components/templates/vjhp/TheHeaderSection.vue';
+import TheSection2 from '~/components/templates/vjhp/TheSection2.vue';
+import TheGamesBrickSection from '~/components/templates/vjhp/TheGamesBrickSection.vue';
+import TheSection4 from '~/components/templates/vjhp/TheSection4.vue';
+import TheSection5 from '~/components/templates/vjhp/TheSection5.vue';
+import TheTestimonialSection from '~/components/templates/vjhp/TheTestimonialSection.vue';
+import TheSection7 from '~/components/templates/vjhp/TheSection7.vue';
+import TheFooterSection from '~/components/templates/vjhp/TheFooterSection.vue';
+
+export default {
+  layout: 'vjhp',
+  head() {
+    return { 
+      title: 'Vera&John - The fun online casino',
+      htmlAttrs: {
+        lang: 'ja',
+      },
+       bodyAttrs: {
+        id: 'ja-jp',
+      }
+    }
+  },
+  components: {
+    TheLoginHeaderSection,
+    TheHeaderSection,
+    TheSection2,
+    TheGamesBrickSection,
+    TheSection4,
+    TheSection5,
+    TheTestimonialSection,
+    TheSection7,
+    TheFooterSection,
+  },
+  async asyncData() {
+    try {
+      const post = await import(
+        '~/assets/content/landing-page/marketing/vjhp/vera-john-homepage.md'
+      );
+      return {
+        post,
+      };
+    } catch (error) {
+      return false;
+    }
+  },
+};
+</script>
